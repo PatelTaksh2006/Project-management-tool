5b35890c033c179394d75aec4d50593cfaf0b998
PatelTaksh2006 <takshk2006@gmail.com>
2025-08-19 12:41:56 +0530

    19-08-2025_Lab

--- proman/src/App.js
+++ proman/src/App.js
@@ -5,7 +5,7 @@ import Manager_Project from "./pages/Manager/Project";
 function App() {
   return (
     <div className="App">
-      <Manager_Dashboard   />
+      {/* <Manager_Dashboard   /> */}
       <Manager_Project />
     </div>
   );
---
+++ proman/src/components/Search_form.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+export default function search_form() {
+  return (
+    <div className="d-flex flex-row">
+      <Form className="d-flex">
+        <Form.Group className="mb-0 me-2" controlId="formBasicSearch">
+          {/* <Form.Label>search</Form.Label> */}
+          <Form.Control
+            type="text"
+            placeholder="search by name"
+            style={{ width: "600px" }}
+          />
+        </Form.Group>
+        <Button type="submit">Search</Button>
+      </Form>
+    </div>
+  );
+}
